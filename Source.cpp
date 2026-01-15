333c8460b9ba99b2913c27d2d2a1dfec475f2f28
hxnadoan <imhanadoan@gmail.com>
2026-01-14 16:48:24 -0800

    branch 3 update

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,14 @@
 // Hana Doan and Michelle Avilez
 // Description: Hello World in C++
 //============================================================
-
+/*
+* hello2Function.cpp
+*
+*/
 #include <iostream>
 using namespace std;
-
-int hello1Function(string name1);
-int hello2Function(string name2);
-
-int main()
+int hello2Function(string name2)
 {
-    string name1="your name1";
-    hello1Function(name1);
-    
-    string name2="your name2";
-    hello2Function(name2);
-    return 0;
+cout << "hello World " << name2 << endl;
+return 0;
 }
