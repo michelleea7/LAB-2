1d3a22d533f88f12ef10e9ed1e1d19cb80e7139f
hxnadoan <imhanadoan@gmail.com>
2026-01-14 16:44:19 -0800

    part 1 update

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,19 @@
+// Hana Doan and Michelle Avilez
+// Description: Hello World in C++
+//============================================================
+
 #include <iostream>
-using namespace std; 
+using namespace std;
+
+int hello1Function(string name1);
+int hello2Function(string name2);
 
 int main()
 {
-int num 
-
-num = 1 + 2;
-
-cout << num; 
-
-    return 0; 
+    string name1="your name1";
+    hello1Function(name1);
+    
+    string name2="your name2";
+    hello2Function(name2);
+    return 0;
 }
